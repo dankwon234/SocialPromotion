53505efc75c263df4c592612ee5ab6dd5f669381
dankwon234 <dan.kwon234@gmail.com>
2016-02-07 02:41:32 -0500

    post route for profile creation

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var Profile = require('../models/Profile');
 
 router.get('/:resource', function(req, res, next) {
 
@@ -13,6 +14,41 @@ router.get('/:resource', function(req, res, next) {
 	}
 
 
+
+});
+
+router.post('/:resource', function(req, res, next) {
+
+	var resource = req.params.resource;
+
+	if (resource == 'profile'){
+
+		var profileInfo = req.body;
+		var name = profileInfo.name;
+		var parts = name.split(' ');
+		var firstName = parts[0];
+		var lastName = (parts.length > 1) ? parts[1] : '';
+		profileInfo['firstName'] = firstName;
+		profileInfo['lastName'] = lastName;
+
+		Profile.create(profileInfo, function(err, profile){
+			if (err){
+				res.json({
+					confirmation:'fail',
+					resource: err.message
+				});
+
+				return;
+			}
+
+			res.json({
+				confirmation: 'success',
+				resource: profile.summary()				
+			});
+
+		});
+
+	}
 
 });
 
