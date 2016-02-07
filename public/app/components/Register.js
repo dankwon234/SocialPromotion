eebd83ad2d75f61036df94dbf1bc7570f851447c
dankwon234 <dan.kwon234@gmail.com>
2016-02-07 02:17:56 -0500

    moved registration functionality to Register component

---
+++ public/app/components/Register.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import SPActionCreator from '../actions/SPActionCreator'
+
+class Register extends React.Component {
+
+	constructor(props, context) {
+		super(props, context);
+		this.updateVisitor = this.updateVisitor.bind(this);
+		this.signUp = this.signUp.bind(this);
+		this.selectLocation = this.selectLocation.bind(this);
+		this.state = {
+			visitor: {
+				name: '',
+				email: '',
+				city: 'new york'
+			}
+		};
+	}
+
+	signUp(event){
+		event.preventDefault();
+		console.log('Sign Up: '+JSON.stringify(this.state.visitor));
+		if (this.state.visitor.name.length == 0){
+			alert('Please enter your NAME')
+			return
+		}
+
+		if (this.state.visitor.email.length == 0){
+			alert('Please enter your EMAIL')
+			return
+		}
+
+		SPActionCreator.registerProfile(this.state.visitor);
+	}
+
+	selectLocation(event){
+		this.state.visitor['location'] = event.target.value;
+		console.log('Select Location: '+JSON.stringify(this.state.visitor));
+	}
+
+	updateVisitor(event){
+		this.state.visitor[event.target.id] = event.target.value;
+	}
+
+	render(){
+		return (
+			<div className="front-panel">
+				<h2>Apply</h2>
+				<hr />
+				Apply for our beta and be among the first on the platform when we launch in Spring.
+				<input onChange={this.updateVisitor} id="name" type="text" placeholder="Name" style={{background:'#f9f9f9', borderRadius:3, marginTop:14}} className="form-control" />
+				<input onChange={this.updateVisitor} id="email" type="text" placeholder="Email" style={{background:'#f9f9f9', borderRadius:3, marginTop:9}} className="form-control" />
+				<select onChange={this.selectLocation} className="form-control" style={{background:'#f9f9f9', height:36, marginBottom:32}}>
+					<option>New York</option>
+					<option>Boston</option>
+					<option>San Francisco</option>
+				</select>
+				<a onClick={this.signUp} href="#" className="btn-primary btn btn-small button btn-round xs-margin-bottom-five">Submit</a>
+			</div>
+
+		);
+	}
+
+}
+
+export default Register;
--- public/app/components/pages/Home.js
+++ public/app/components/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import SectionCard from '../../components/SectionCard'
 import PostCard from '../../components/PostCard'
+import Register from '../../components/Register'
 import SPActionCreator from '../../actions/SPActionCreator'
 
 class Home extends React.Component {
@@ -8,47 +9,14 @@ class Home extends React.Component {
 	constructor(props, context) {
 //		console.log('Home: constructor');
 		super(props, context);
-		this.updateVisitor = this.updateVisitor.bind(this);
-		this.signUp = this.signUp.bind(this);
-		this.selectLocation = this.selectLocation.bind(this);
 		this.state = {
 			posts: [
 				{id:1, type:'Job', title:'Hiring Software Intern', company:'Bedrocket Media', description:'This is the description. This is the description. This is the description. This is the description. This is the description.', location:'NYC', tags:['programming', 'Angular JS'], image:'catherine.png'},
 				{id:2, type:'Rental', title:'Apartment Available', company:'', description:'This is the description. This is the description. This is the description. This is the description. This is the description.', location:'NYC', tags:['programming', 'Angular JS'], image:'catherine.png'},
 				{id:3, type:'Event', title:'Startup Investing Workshop', company:'Bedrocket Media', description:'This is the description. This is the description. This is the description. This is the description. This is the description.', location:'NYC', tags:['programming', 'Angular JS'], image:'catherine.png'},
-				{id:4, type:'General', title:'Software Intern', company:'Bedrocket Media', description:'This is the description. This is the description. This is the description. This is the description. This is the description.', location:'NYC', tags:['programming', 'Angular JS'], image:'catherine.png'}
-			],
-			visitor: {
-				name: '',
-				email: '',
-				city: 'new york'
-			}
+				{id:4, type:'Misc', title:'Software Intern', company:'Bedrocket Media', description:'This is the description. This is the description. This is the description. This is the description. This is the description.', location:'NYC', tags:['programming', 'Angular JS'], image:'catherine.png'}
+			]
 		};
-	}
-
-	signUp(event){
-		event.preventDefault();
-		console.log('Sign Up: '+JSON.stringify(this.state.visitor));
-		if (this.state.visitor.name.length == 0){
-			alert('Please enter your NAME')
-			return
-		}
-
-		if (this.state.visitor.email.length == 0){
-			alert('Please enter your EMAIL')
-			return
-		}
-
-		SPActionCreator.registerProfile(this.state.visitor);
-	}
-
-	selectLocation(event){
-		this.state.visitor['location'] = event.target.value;
-		console.log('Select Location: '+JSON.stringify(this.state.visitor));
-	}
-
-	updateVisitor(event){
-		this.state.visitor[event.target.id] = event.target.value;
 	}
 
 
@@ -80,19 +48,8 @@ class Home extends React.Component {
 				        	</div>
 
 				        	<div className="col-md-4 col-md-push-3">
-				        		<div className="front-panel">
-				        			<h2>Apply</h2>
-				        			<hr />
-									Apply for our beta and be among the first on the platform when we launch in Spring.
-				        			<input onChange={this.updateVisitor} id="name" type="text" placeholder="Name" style={{background:'#f9f9f9', borderRadius:3, marginTop:14}} className="form-control" />
-				        			<input onChange={this.updateVisitor} id="email" type="text" placeholder="Email" style={{background:'#f9f9f9', borderRadius:3, marginTop:9}} className="form-control" />
-				        			<select onChange={this.selectLocation} className="form-control" style={{background:'#f9f9f9', height:36, marginBottom:32}}>
-				        				<option>New York</option>
-				        				<option>Boston</option>
-				        				<option>San Francisco</option>
-				        			</select>
-									<a onClick={this.signUp} href="#" className="btn-primary btn btn-small button btn-round xs-margin-bottom-five">Submit</a>
-				        		</div>
+				        		<Register />
+
 				        	</div>
 			        	</div>
 		        	</div>
@@ -109,19 +66,7 @@ class Home extends React.Component {
 		                        	partnerships, events, rentals and more. By curating our membership, we ensure that 
 		                        	all content on SP is a legitimate opportunity with high performing individuals.
 		                        </p>
-				        		<div className="front-panel">
-				        			<h2>Apply</h2>
-				        			<hr />
-									Apply for our beta and be among the first on the platform when we launch in Spring.
-				        			<input onChange={this.updateVisitor} id="name" type="text" placeholder="Name" style={{background:'#f9f9f9', borderRadius:3, marginTop:14}} className="form-control" />
-				        			<input onChange={this.updateVisitor} id="email" type="text" placeholder="Email" style={{background:'#f9f9f9', borderRadius:3, marginTop:9}} className="form-control" />
-				        			<select onChange={this.selectLocation} className="form-control" style={{background:'#f9f9f9', height:36, marginBottom:32}}>
-				        				<option>New York</option>
-				        				<option>Boston</option>
-				        				<option>San Francisco</option>
-				        			</select>
-									<a onClick={this.signUp} href="#" className="btn-primary btn btn-small button btn-round xs-margin-bottom-five">Submit</a>
-				        		</div>
+				        		<Register />
 		                    </div>
 
 		                    <div className="col-md-7 col-md-offset-1">
