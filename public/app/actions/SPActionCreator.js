d87e4eb5354833284e4fc4b51a62e4e4ed70d390
dankwon234 <dan.kwon234@gmail.com>
2016-02-07 02:49:47 -0500

    make api call when profile registers

--- public/app/actions/SPActionCreator.js
+++ public/app/actions/SPActionCreator.js
@@ -1,10 +1,11 @@
 import Dispatcher from '../dispatcher/Dispatcher'
+import APIUtils from '../utils/APIUtils'
 
 export default {
 
 	registerProfile(profile){
-		console.log('registerProfile: '+JSON.stringify(profile));
-
+//		console.log('registerProfile: '+JSON.stringify(profile));
+		APIUtils.registerProfile(profile)
 	}
 
 
