84d9fa12bc61727211a8b53d8f19e5257c9e22b2
dankwon234 <dan.kwon234@gmail.com>
2016-02-07 02:42:09 -0500

    api utils

---
+++ public/app/utils/APIUtils.js
@@ -0,0 +1,45 @@
+import Promise from 'bluebird'
+import superagent from 'superagent'
+
+function urlRequest(method, path, params){
+    return new Promise(function (resolve, reject){
+
+    	if (method.toLowerCase() == 'get'){
+			superagent
+				.get(path)
+				.query(params)
+				.set('Accept', 'application/json')
+				.end(function(err, res){
+					if (err){ reject(err); }
+					else { resolve(res.body); }
+				});
+    	}
+
+    	if (method.toLowerCase() == 'post'){
+			superagent
+				.post(path)
+				.send(params)
+				.set('Accept', 'application/json')
+				.end(function(err, res){
+					if (err){ reject(err); }
+					else { resolve(res.body); }
+				});
+    	}
+    });
+}
+
+export default {
+
+	registerProfile(profile) {
+//		console.log('APIUtils - registerProfile: '+JSON.stringify(profile));
+		urlRequest('post', '/api/profile', profile)
+		.then(function(response){
+			console.log('API UTILS - RESPONSE: '+JSON.stringify(response));
+//			FullStackServerActionCreator.userLoggedIn(response.profile);
+		})
+		.catch(function(err){
+			console.log('ERROR: '+err.message);
+		});
+
+	}
+}
